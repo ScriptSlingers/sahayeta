f9c9dcdeeead73390a132beacb39ecf4e483f124
anjalipoudel <sabnom77poudel@gmail.com>
2023-09-26 17:48:27 +0545

    project setup

---
+++ src/app/api/route.ts
@@ -0,0 +1,7 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  return NextResponse.json({
+    name: `Hello, from ${request.url} I'm now an Edge Function!`
+  });
+}
